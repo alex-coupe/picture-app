2babc5f9dbf1fa6f7519e29a830bad5b86e652c0
Alex <acooupe@gmail.com>
2020-04-26 15:11:34 +0100

    Styling the image card

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -5,23 +5,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ImageCard : React.FC<Image> = ({webformatURL,tags, user, likes, comments, favorites, downloads, views}) => {
     
-    const splitTags:string[] = tags.split(',');
+    const splitTags:string[] = tags.split(', ');
 
     return (
         <div className="card">
             <img src={webformatURL} alt="caption" className="column-image" />
-            <div className="m-3">
-                <span className="mr-2 text-primary"><FontAwesomeIcon size="2x" icon={faCamera} /> {user}</span>
-                <span className="mr-2"><FontAwesomeIcon size="2x" icon={faDownload} /> {downloads}</span>
-                <span className="mr-2"><FontAwesomeIcon size="2x" icon={faBinoculars} /> {views}</span>
-                <span className="mr-2"><FontAwesomeIcon size="2x" icon={faHeart} /> {likes}</span>
-                <span className="mr-2"><FontAwesomeIcon size="2x" icon={faComments} /> {comments}</span>
-                <span className="mr-2"><FontAwesomeIcon size="2x" icon={faStar} /> {favorites}</span>
+            <div>
+                <span className="mr-2 text-left"><FontAwesomeIcon size="2x" icon={faCamera} /> <span className="text-primary"><b>{user}</b></span></span>
+               
+                    <span className="mr-2"><FontAwesomeIcon size="2x" icon={faDownload} /> <span className="text-primary"><b>{downloads}</b></span></span>
+                    <span className="mr-2"><FontAwesomeIcon size="2x" icon={faBinoculars} /> <span className="text-primary"><b>{views}</b></span></span>
+                
+                <div className="mt-1">
+                    <span className="mr-2"><FontAwesomeIcon size="2x" icon={faHeart} /> <span className="text-primary"><b>{likes}</b></span></span>
+                    <span className="mr-2"><FontAwesomeIcon size="2x" icon={faComments} /> <span className="text-primary"><b>{comments}</b></span></span>
+                    <span className="mr-2"><FontAwesomeIcon size="2x" icon={faStar} /> <span className="text-primary"><b>{favorites}</b></span></span>
+                </div>
+                <div className="mt-2 mb-1">
                 {splitTags.map( (tag, index) => {
                     return (
-                        <span className="badge" key={index}>{tag}</span>
+                        <span className="badge" key={index}>#{tag}</span>
                     )
                 })}
+                </div>
             </div>
         </div>
     )
